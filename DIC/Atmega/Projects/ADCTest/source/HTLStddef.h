51eb736461ebcb3b921b482cd7da604e09dfc1e6
Andrej Tica <andrejtica@gmail.com>
2020-10-01 11:06:26 +0200

    New stuff again

---
+++ DIC/Atmega/Projects/ADCTest/source/HTLStddef.h
@@ -0,0 +1,17 @@
+/*
+ * HTLStddef
+ *
+ * Created: 29.05.2019 14:37:36
+ * Author : ingom
+ */ 
+
+#ifndef HTLSTDDEV
+#define HTLSTDDEV
+
+typedef enum
+{
+	EFALSE = 0,
+	ETRUE = 1
+} TBool;
+
+#endif
